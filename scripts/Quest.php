97641f58845576517c08f37a9cf76dd2914b977f
magnusmanske <tools.quest@tools-sgebastion-10.tools.eqiad1.wikimedia.cloud>
2022-07-27 12:26:56 +0000

    first light

---
+++ scripts/Quest.php
@@ -0,0 +1,14 @@
+<?PHP
+
+require_once ( '/data/project/wikidata-todo/public_html/php/ToolforgeCommon.php' ) ;
+//require_once ( '/data/project/wikidata-todo/public_html/php/wikidata.php' ) ;
+//require_once ( '/data/project/quickstatements/public_html/quickstatements.php' ) ;
+
+class Quest {
+	function __construct () {
+		$this->tfc = new ToolforgeCommon ;
+	}
+}
+
+
+?>
---
+++ scripts/test.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php
+<?PHP
+
+require_once ( '/data/project/quest/scripts/Quest.php' ) ;
+
+$quest = new Quest ;
+
+?>
